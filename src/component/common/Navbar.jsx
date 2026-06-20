3bc57e8683ec3f47f48f9ca2523fecd1c1ad4d31
Tuan <nguyenhahongtuan@gmail.com>
2026-06-21 05:10:08 +0700

    feat: Add Navbar component for navigation and user authentication

---
+++ src/component/common/Navbar.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import ApiService from "../../service/ApiService";
+
+function Navbar() {
+  const isAuthenticated = ApiService.isAuthenticated();
+  const isAdmin = ApiService.isAdmin();
+  const isUser = ApiService.isUser();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    const isLogout = window.confirm("Are you sure you want to logout?");
+    if (isLogout) {
+      ApiService.logout();
+      navigate("/home");
+    }
+  };
+
+  return (
+    <nav>
+      <div className="navbar-brand">
+        <NavLink to="/home">Hotel Booking</NavLink>
+      </div>
+      <ul className="navbar-ul">
+        <li>
+          <NavLink to="/home" activeClass="active">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/rooms" activeClass="active">
+            Rooms
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/find-booking" activeClass="active">
+            Find My Booking
+          </NavLink>
+        </li>
+
+        {isAuthenticated && (
+          <li>
+            <NavLink to="/profile" activeClass="active">
+              Profile
+            </NavLink>
+          </li>
+        )}
+        {isAdmin && (
+          <li>
+            <NavLink to="/admin" activeClass="active">
+              Admin
+            </NavLink>
+          </li>
+        )}
+
+        {!isAuthenticated && (
+          <>
+            <li>
+              <NavLink to="/login" activeClass="active">
+                Login
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/register" activeClass="active">
+                Register
+              </NavLink>
+            </li>
+          </>
+        )}
+
+        {isAuthenticated && <li onClick={handleLogout}>Logout</li>}
+      </ul>
+    </nav>
+  );
+}
